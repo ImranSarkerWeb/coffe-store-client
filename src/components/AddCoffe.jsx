59fbef87d880627e117a9a832188b07e25f3b346
imransarkerweb <imransarker.web@gmail.com>
2023-05-11 00:51:40 +0600

    basic components added

---
+++ src/components/AddCoffe.jsx
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+const AddCoffe = () => {
+  return <div>AddCoffe</div>;
+};
+
+export default AddCoffe;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AddCoffe from "./components/AddCoffe.jsx";
+import UpdateCoffe from "./components/UpdateCoffe.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/addCoffe",
+    element: <AddCoffe></AddCoffe>,
+  },
+  {
+    path: "/updateCoffe",
+    element: <UpdateCoffe></UpdateCoffe>,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
